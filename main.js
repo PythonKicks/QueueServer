0741e034f0f1d51c56af76d58004738a332566e0
PythonKicks <pythonkicks@gmail.com>
2020-04-28 15:26:39 -0400

    Removed dotenv

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const http = require('http');
 global.cookieParser;
 global.cookieParser = require('cookie-parser');
 
 const app = require('./app');
 const attachGameSocket = require('./gameSocket');
-
 const PORT = process.env.PORT || 5000;
 const server = http.createServer({}, app);
 attachGameSocket(server);
